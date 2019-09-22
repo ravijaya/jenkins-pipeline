954344062bfc0c064ec827a856c9e69d74c30cd2
Gabriel N Schenker <gnschenker@gmail.com>
2019-09-22 16:48:03 +0200

    Final version

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,6 +2,8 @@
 
 echo "Removing api container if it exists..."
 docker container rm -f api || true
+echo "Removing network test-net if it exists..."
+docker network rm test-net || true
 
 echo "Deploying app ($registry:$BUILD_NUMBER)..."
 docker network create test-net
@@ -11,14 +13,14 @@ docker container run -d \
     --net test-net \
     $registry:$BUILD_NUMBER
 
+# Logic to wait for the api component to be ready on port 3000
+
 read -d '' wait_for << EOF
 echo "Waiting for API to listen on port 3000..."
-
 while ! nc -z api 3000; do   
   sleep 0.1 # wait for 1/10 of the second before check again
   printf "."
 done
-
 echo "API ready on port 3000!"
 EOF
 
--- jenkins/scripts/wait-for.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-echo "Waiting for API to listen on port 3000..."
-
-while ! nc -z api 3000; do   
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
-
-echo "API ready on port 3000!"
